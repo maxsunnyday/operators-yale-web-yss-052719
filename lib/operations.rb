438397eed545e63c0aff85b3b873d2ebdd902c6c
Max Sun <max.sun@yale.edu>
2019-05-15 00:04:45 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,13 @@
 def unsafe?(speed)
 	if speed < 40 | speed > 60
-		true
+		return true
 	else
-		false
+		return false
 	end
 end
 
 
 
 def not_safe?(speed)
-	speed < 40 | speed > 60 ? true : false
+	speed < 40 | speed > 60 ? return true : return false
 end
